4bc00201b7a017ad18fb480e0a60641887640271
Ian Price <michael.ian.price@gmail.com>
2017-06-08 23:30:06 -0400

    filtering bad requests

--- index.js
+++ index.js
@@ -24,11 +24,52 @@ fs.readJson('bookmarks.json')
             // console.log(f);
             return f.children;
         });
-        let root_folder_children_counts = root_folder_children.map(f => {
-            return f.length;
+        let master_links_array = [];
+        root_folder_names.forEach((element,idx) => {
+            collect_links(master_links_array, element, root_folder_children[idx]);
         });
+        let requests_setup = master_links_array.map(link => {
+            // console.log(link);
+            let split_link = link.split('|||');
+            return {
+                dir_string: split_link[0],
+                url: split_link[1]
+            };
+        });
+        requests_setup = requests_setup.filter((element) => {
+            return !(_.includes(element.url,'chrome://') || _.includes(element.url,'localhost') || _.includes(element.url,'ftp://') || _.includes(element.url,'whichasteroidbroughtmehere.com') || _.includes(element.url,'50.23.99.148') || _.includes(element.url,'algorithmsandme'));
+        });
+        console.log(requests_setup.length);
+        // let root_folder_children_counts = root_folder_children.map(f => {
+        //     return f.length;
+        // });
         // console.log(roots);
-        console.log(root_folder_names);
-        console.log(root_folder_children_counts);
+        // console.log(root_folder_names);
+        // console.log(root_folder_children_counts);
+        return Promise.mapSeries(requests_setup, element => {
+            return Promise.all([
+                element,
+                rp({ uri: element.url, simple: false })
+            ]);
+        });
+    })
+    .then(results => {
+        let responses = {};
+        results.forEach(element => {
+            console.log(element[1].response);
+        })
     })
     .catch(err => console.log(err));
+
+
+function collect_links(master_arr, dir_string, folder_set) {
+    folder_set.forEach((element, idx) => {
+        if (element.type === 'url') {
+            master_arr.push(`${dir_string}___${idx.toString()}|||${element.url}`)
+        } else if (element.type === 'folder') {
+            collect_links(master_arr, `${dir_string}___${idx.toString()}___${element.name}`, element.children);
+        } else {
+            console.log(`Found something else... ${element.type}`);
+        }
+    });
+}
